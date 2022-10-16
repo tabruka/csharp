7b13cedad75c06fb4fbf4639315fda7b4f36932c
Aleksandr <tabruka@yandex.ru>
2022-10-16 13:59:04 +0300

    Example5_HelloUsers

---
+++ Example5_HelloUsers/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
+// To.Lower 
